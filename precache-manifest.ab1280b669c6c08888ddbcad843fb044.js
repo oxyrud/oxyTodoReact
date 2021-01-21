dd630f0fb76199ec30bfec090bf3ff6b39c83183
Oxana <62831704+oxyrud@users.noreply.github.com>
2021-01-21 18:44:54 +0300

    Updates

---
+++ precache-manifest.ab1280b669c6c08888ddbcad843fb044.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfdaff0a3f94fa6f8743be246d0c80b",
+    "url": "/oxyrudTodoReact/index.html"
+  },
+  {
+    "revision": "cb705e65ac26314605db",
+    "url": "/oxyrudTodoReact/static/css/main.51fa5916.chunk.css"
+  },
+  {
+    "revision": "47b07bac4324e74ca2af",
+    "url": "/oxyrudTodoReact/static/js/2.f0e84c41.chunk.js"
+  },
+  {
+    "revision": "9230f7c4dbd642e80c48e1240613b9a9",
+    "url": "/oxyrudTodoReact/static/js/2.f0e84c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb705e65ac26314605db",
+    "url": "/oxyrudTodoReact/static/js/main.010a2ee7.chunk.js"
+  },
+  {
+    "revision": "0fb7e844dc9e1024f360",
+    "url": "/oxyrudTodoReact/static/js/runtime-main.7cd3e2f0.js"
+  },
+  {
+    "revision": "b7c30d82259e465959d40aa8fa6cd79e",
+    "url": "/oxyrudTodoReact/static/media/Fork.b7c30d82.svg"
+  },
+  {
+    "revision": "dc001e86e702bf751aff1216cd81b11f",
+    "url": "/oxyrudTodoReact/static/media/WHS_black.dc001e86.svg"
+  },
+  {
+    "revision": "584b642af754da2e11999cf7340c3d3e",
+    "url": "/oxyrudTodoReact/static/media/facebook.584b642a.svg"
+  },
+  {
+    "revision": "3feade3d4db28bbf7d006510b64309a4",
+    "url": "/oxyrudTodoReact/static/media/github.3feade3d.svg"
+  },
+  {
+    "revision": "203861361e85bacc72496817f45e713f",
+    "url": "/oxyrudTodoReact/static/media/mail.20386136.svg"
+  },
+  {
+    "revision": "a3b344a246a1de75f22227e0e72f5216",
+    "url": "/oxyrudTodoReact/static/media/star.a3b344a2.svg"
+  },
+  {
+    "revision": "4073458ba52319c0395010aac24b1687",
+    "url": "/oxyrudTodoReact/static/media/telegram.4073458b.svg"
+  },
+  {
+    "revision": "15df3a30fea0cfe664ee37d4c92ac060",
+    "url": "/oxyrudTodoReact/static/media/thomas-heintz-0tgMnMIYQ9Y-unsplash.15df3a30.jpg"
+  }
+]);
